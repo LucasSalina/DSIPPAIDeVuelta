7068b9ecfcf58dbe053863cdb496a8143f407a3f
Daniel Dragún <draguncarreras@gmail.com>
2025-06-04 13:42:16 -0300

    Agregando ordenarPorFechaHoraOcurrencia los datosPrincipales de los eventos sismicos no revisados.

--- DSI_TP1/src/main/java/com/grupo7/dsi_tp1/GestorRevisionManual.java
+++ DSI_TP1/src/main/java/com/grupo7/dsi_tp1/GestorRevisionManual.java
@@ -3,6 +3,8 @@ package com.grupo7.dsi_tp1;
 // Dependencies
 import java.util.List; 
 import java.util.ArrayList;
+import java.time.LocalDateTime;
+import java.util.Comparator;
 
 // Entities
 import com.grupo7.dsi_tp1.EventoSismico;
@@ -18,7 +20,6 @@ public class GestorRevisionManual {
     
     // Constructor
     public GestorRevisionManual(List<EventoSismico> eventosSismicos, List<Estado> estados, List<Usuario> usuarios) {
-        
         this.eventosSismicos = eventosSismicos;
         this.estados = estados; 
         this.usuarios = usuarios; 
@@ -26,25 +27,32 @@ public class GestorRevisionManual {
 
     // Comportamiento
     public List<List<String>> buscarEventosSismicosNoRevisados() {
-        
         // Inicializando la matriz de datos principales de eventos sismicos no revisados
         List<List<String>> eventosSismicosNoRevisados = new ArrayList<>();
         
         // Recorriendo todos los eventos sismicos 
         for (EventoSismico eventoSismico : eventosSismicos) {
-        
             // Revisando si el evento sismico es no revisado
             if (eventoSismico.esAutoDetectadoOPendienteRevision()) {
-                
-                // Agregando el evento sismico al la matriz de datos registrados
+                // Agregando el evento sismico a la matriz de datos registrados
                 eventosSismicosNoRevisados.add(eventoSismico.obtenerDatosPrincipales());
             }
-            
         }
         
-        // Devolviendo la matriz de datos principales
-        return eventosSismicosNoRevisados;
+        // Devolviendo la matriz de datos principales ordenados
+        return ordenarPorFechaHoraOcurrencia(eventosSismicosNoRevisados);
     }
     
-    
+    public List<List<String>> ordenarPorFechaHoraOcurrencia(List<List<String>> datosPrincipales) {
+        // Ordenamos en sitio (ascendente) según el campo 0 parseado a LocalDateTime
+        datosPrincipales.sort(new Comparator<List<String>>() {
+            @Override
+            public int compare(List<String> o1, List<String> o2) {
+                LocalDateTime dt1 = LocalDateTime.parse(o1.get(0));
+                LocalDateTime dt2 = LocalDateTime.parse(o2.get(0));
+                return dt1.compareTo(dt2);
+            }
+        });
+        return datosPrincipales;
+    }
 }
